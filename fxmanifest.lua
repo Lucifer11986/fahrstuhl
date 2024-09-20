52dc8a66e408f4edd4aaaa27fb89ea9138391f89
Lucifer11986 <dmenter21@googlemail.com>
2024-09-20 11:06:44 +0200

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,15 @@
+fx_version 'cerulean'
+games {'gta5'}
+
+author 'Lucifer'
+description 'Fahrstuhl-Skript'
+version '1.0.0'
+
+client_scripts {
+    'config.lua',
+    'client.lua'
+}
+
+server_scripts {
+    'server.lua'
+}
